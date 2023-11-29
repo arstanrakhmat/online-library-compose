8dcf80fc625e4d49097006a6830afbe193e30155
Arstanbek Rakhmatulaev <rakhmatulaev_a@auca.kg>
2023-11-29 21:02:56 +0300

    Used navigation to show splash screen

--- app/src/main/java/com/example/onlinelibrary/screens/ReaderSplashScreen.kt
+++ app/src/main/java/com/example/onlinelibrary/screens/ReaderSplashScreen.kt
@@ -1,8 +1,10 @@
 package com.example.onlinelibrary.screens
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
 
 @Composable
-fun ReaderSplashScreen() {
-
+fun ReaderSplashScreen(navController: NavController) {
+    Text(text = "SplashScreen")
 }
