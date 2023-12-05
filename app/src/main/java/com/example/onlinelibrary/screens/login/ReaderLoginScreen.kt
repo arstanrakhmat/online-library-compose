82fa75c4317409359703bff6ce7e073b658bb876
Arstanbek Rakhmatulaev <rakhmatulaev_a@auca.kg>
2023-12-05 13:15:32 +0300

    Add design for splash screen

--- app/src/main/java/com/example/onlinelibrary/screens/login/ReaderLoginScreen.kt
+++ app/src/main/java/com/example/onlinelibrary/screens/login/ReaderLoginScreen.kt
@@ -1,8 +1,10 @@
 package com.example.onlinelibrary.screens.login
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
 
 @Composable
-fun ReaderLoginScreen() {
-
+fun ReaderLoginScreen(navController: NavController) {
+    Text(text = "Login screen")
 }
